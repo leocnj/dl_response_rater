c9e2925084494d4ef34d73640d4514c39e0ed61c
Chen, Lei <LChen@ETS.ORG>
2015-12-10 14:38:31 -0500

    sg15 init.
    - added pd_load.py to create two cvs files for the sg15 data set.
    - created cnn1d_sg15.py to use pre-defined train/test to run 1D-CNN

---
+++ cnn1d_sg15.py
@@ -0,0 +1,91 @@
+from __future__ import print_function
+
+import pandas as pd
+import numpy as np
+
+from keras.preprocessing import sequence
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation, Flatten
+from keras.layers.embeddings import Embedding
+from keras.layers.convolutional import Convolution1D, MaxPooling1D
+from keras.preprocessing.text import Tokenizer
+from keras.callbacks import EarlyStopping
+from keras.utils import np_utils
+
+"""
+following https://gist.github.com/xccds/8f0e5b0fe4eb6193261d to do 1d-CNN sentiment detection on the mr data.
+
+- use data/train|test.csv to load txt|score two-col DF
+
+"""
+def df2seq(df, nb_words):
+
+    textraw = df.txt.values.tolist()
+    textraw = [line.encode('utf-8') for line in textraw]  # keras needs str
+
+    # keras deals with tokens
+    token = Tokenizer(nb_words=nb_words)
+    token.fit_on_texts(textraw)
+    text_seq = token.texts_to_sequences(textraw)
+    return(text_seq, df.score.values)
+
+
+np.random.seed(1337)  # for reproducibility
+
+# load csv to PD
+train_csv = pd.read_csv('data/train.csv')
+test_csv = pd.read_csv('data/test.csv')
+
+print(train_csv.head())
+
+nb_words = 20000
+train_X, train_y = df2seq(train_csv, nb_words)
+test_X, test_y = df2seq(test_csv, nb_words)
+nb_classes = len(np.unique(train_y))
+
+# set parameters:
+maxfeatures = nb_words
+max_features = 5000
+maxlen = 200
+batch_size = 32
+embedding_dims = 100
+nb_filter = 100
+filter_length = 5
+hidden_dims = 100
+nb_epoch = 20
+pool_length = 10
+
+# padding
+
+print("Pad sequences (samples x time)")
+X_train = sequence.pad_sequences(train_X, maxlen=maxlen, padding='post', truncating='post')
+X_test = sequence.pad_sequences(test_X, maxlen=maxlen, padding='post', truncating='post')
+print('X_train shape:', X_train.shape)
+print('X_test shape:', X_test.shape)
+
+Y_train = np_utils.to_categorical(train_y, nb_classes)
+Y_test = np_utils.to_categorical(test_y, nb_classes)
+
+print('Build model...')
+model = Sequential()
+
+model.add(Embedding(maxfeatures, embedding_dims, input_length=maxlen))
+model.add(Dropout(0.25))
+model.add(Convolution1D(nb_filter=nb_filter,
+                        filter_length=filter_length,
+                        border_mode="valid",
+                        activation="relu"))
+
+model.add(MaxPooling1D(pool_length=pool_length))
+model.add(Flatten())
+
+model.add(Dense(hidden_dims))
+model.add(Dropout(0.25))
+model.add(Activation('relu'))
+model.add(Dense(nb_classes))
+model.add(Activation('softmax'))
+model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
+
+earlystop = EarlyStopping(monitor='val_loss', patience=1, verbose=1)
+result = model.fit(X_train, Y_train, batch_size=batch_size, nb_epoch=nb_epoch,
+            validation_split=0.1, show_accuracy=True,callbacks=[earlystop])
